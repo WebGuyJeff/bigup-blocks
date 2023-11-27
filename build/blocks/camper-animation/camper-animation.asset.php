fdb6ff0ce323eaa7bba365f4f1539a093a6e689b
wordpressjeff <me@jeffersonreal.com>
2023-11-27 12:08:06 +0000

    fix overflow on camper wrapper

--- build/blocks/camper-animation/camper-animation.asset.php
+++ build/blocks/camper-animation/camper-animation.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'wp-block-editor', 'wp-blocks', 'wp-i18n'), 'version' => '671ba02802e89971cee5');
+<?php return array('dependencies' => array('react', 'wp-block-editor', 'wp-blocks', 'wp-i18n'), 'version' => 'f41ae7d870cbada3b0c6');
