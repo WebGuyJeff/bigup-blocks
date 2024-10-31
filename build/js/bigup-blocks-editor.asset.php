18d37b5ef531097ce3eabcd85fd5cc81c264889d
BigupJeff <me@jeffersonreal.uk>
2024-10-31 13:04:35 +0000

    remove white background

--- build/js/bigup-blocks-editor.asset.php
+++ build/js/bigup-blocks-editor.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'wp-blocks', 'wp-i18n'), 'version' => '53a967ddb213b121082c');
+<?php return array('dependencies' => array('react', 'wp-blocks', 'wp-i18n'), 'version' => '55b5047effa28d3ed5bb');
